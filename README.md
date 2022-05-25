c9fcd9ac6c4856f17b8aebec5817c6a020bff1ab
Yoshiboi18303 <yoshiboi18303.t@gmail.com>
2022-05-25 00:48:55 +0000

    Fixed Publishing Problem (hopefully)

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# Radar.js
+# Radar-js
+
 An unofficial package used to interact with the [Radar Bot Directory](https://radarbotdirectory.xyz) API
 
 ---
 
 # Developer
+
 This package was developed by Yoshiboi18303
 
 ---
@@ -11,45 +13,49 @@ This package was developed by Yoshiboi18303
 # Usage
 
 ## Server Stats Post
+
 ```js
 const Discord = require("discord.js");
-const Radar = require("radar.js");
+const Radar = require("radar-js");
 const client = new Discord.Client({
-  intents: Object.values(Discord.Intents.FLAGS)
-})
-const radar = new Radar(client, "Your Radar Bot Directory Token")
+  intents: Object.values(Discord.Intents.FLAGS),
+});
+const radar = new Radar(client, "Your Radar Bot Directory Token");
 
 client.on("ready", async () => {
-  console.log("The client is ready!")
-  console.log(await radar.stats(client.guilds.cache.size))
-})
+  console.log("The client is ready!");
+  console.log(await radar.stats(client.guilds.cache.size));
+});
 
-client.login("Your token goes here")
+client.login("Your token goes here");
 ```
 
 ## Bot Widget
+
 ```js
-console.log(await radar.botWidget())
+console.log(await radar.botWidget());
 ```
 
 ## Autoposting Stats
+
 ```js
 const Discord = require("discord.js");
-const Radar = require("radar.js");
+const Radar = require("radar-js");
 const client = new Discord.Client({
-  intents: Object.values(Discord.Intents.FLAGS)
-})
-const radar = new Radar(client, "Your Radar Bot Directory Token")
+  intents: Object.values(Discord.Intents.FLAGS),
+});
+const radar = new Radar(client, "Your Radar Bot Directory Token");
 
 client.on("ready", async () => {
-  console.log("The client is ready!")
-  console.log(await radar.stats(client.guilds.cache.size), 1, true)
-})
+  console.log("The client is ready!");
+  console.log(await radar.stats(client.guilds.cache.size), 1, true);
+});
 
-client.login("Your token goes here")
+client.login("Your token goes here");
 ```
 
 ---
 
 # Note
+
 This package could be really buggy (as I'm shitty at JS), so any bug reports would be appreciated.
--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const ms = require("ms");
 
 module.exports = class Client {
-  /** 
+  /**
    * @param {Discord.Client} client
    * @param {String} token
    */
@@ -12,10 +12,11 @@ module.exports = class Client {
     this.token = token;
     this.fetch = fetch;
 
-    if(!(this.client instanceof Discord.Client)) throw new Error("The client option needs to be a Discord.js Client!")
+    if (!(this.client instanceof Discord.Client))
+      throw new Error("The client option needs to be a Discord.js Client!");
   }
 
-  /** 
+  /**
    * Posts server stats to the API.
    * Requires the server count, shard count will be set to 1 if not provided.
    * Can autopost to the API if the autopost option is true.
@@ -25,88 +26,108 @@ module.exports = class Client {
    * @param {Boolean} autopost
    */
   stats(serverCount, shardCount, autopost) {
-    if(!serverCount) throw new Error("The server count is required!")
-    if(!shardCount) shardCount = 1
-    if(!autopost) autopost = false;
+    if (!serverCount) throw new Error("The server count is required!");
+    if (!shardCount) shardCount = 1;
+    if (!autopost) autopost = false;
 
     return new Promise(async (resolve, reject) => {
-      if(autopost) {
+      if (autopost) {
         setInterval(async () => {
-          var req = await this.fetch.default(`https://radarbotdirectory.xyz/api/bot/${this.client.user.id}/stats`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": this.token
-            },
-            body: JSON.stringify({
-              guilds: serverCount,
-              shards: shardCount
-            })
-          })
-          if(req.status != 200) return reject(`[RadarJS] - ${req.status}: ${req.statusText}`)
-          var data = await req.json()
-          return resolve(data)
-        }, ms("2m"))
+          var req = await this.fetch.default(
+            `https://radarbotdirectory.xyz/api/bot/${this.client.user.id}/stats`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: this.token,
+              },
+              body: JSON.stringify({
+                guilds: serverCount,
+                shards: shardCount,
+              }),
+            }
+          );
+          if (req.status != 200)
+            return reject(`[RadarJS] - ${req.status}: ${req.statusText}`);
+          var data = await req.json();
+          return resolve(data);
+        }, ms("2m"));
       } else {
-        var req = await this.fetch.default(`https://radarbotdirectory.xyz/api/bot/${this.client.user.id}/stats`, {
+        var req = await this.fetch.default(
+          `https://radarbotdirectory.xyz/api/bot/${this.client.user.id}/stats`,
+          {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
-              "Authorization": this.token
+              Authorization: this.token,
             },
             body: JSON.stringify({
               guilds: serverCount,
-              shards: shardCount
-            })
-          })
-          if(req.status != 200) return reject(`[RadarJS] - ${req.status}: ${req.statusText}`)
-          var data = await req.json()
-          return resolve(data)
+              shards: shardCount,
+            }),
+          }
+        );
+        if (req.status != 200)
+          return reject(`[RadarJS] - ${req.status}: ${req.statusText}`);
+        var data = await req.json();
+        return resolve(data);
       }
-    })
+    });
   }
 
-  /** 
+  /**
    * Fetches your bot info from the API.
    */
   botInfo() {
     return new Promise(async (resolve, reject) => {
-      var req = await this.fetch.default(`https://radarbotdirectory.xyz/api/bot/${this.client.user.id}`, {
-        method: "GET"
-      })
-      if(req.status != 200) return reject(`[RadarJS] - ${req.status}: ${req.statusText}`)
-      var data = await req.json()
-      return resolve(data)
-    })
+      var req = await this.fetch.default(
+        `https://radarbotdirectory.xyz/api/bot/${this.client.user.id}`,
+        {
+          method: "GET",
+        }
+      );
+      if (req.status != 200)
+        return reject(`[RadarJS] - ${req.status}: ${req.statusText}`);
+      var data = await req.json();
+      return resolve(data);
+    });
   }
 
-  /** 
+  /**
    * Fetches your bot widget from the API
    */
   botWidget() {
     return new Promise(async (resolve, reject) => {
-      var req = await this.fetch.default(`https://radarbotdirectory.xyz/api/bot/${this.client.user.id}/widget`, {
-        method: "GET"
-      })
-      if(req.status != 200) return reject(`[RadarJS] - ${req.status}: ${req.statusText}`)
-      var data = await req.json()
-      return resolve(data)
-    })
+      var req = await this.fetch.default(
+        `https://radarbotdirectory.xyz/api/bot/${this.client.user.id}/widget`,
+        {
+          method: "GET",
+        }
+      );
+      if (req.status != 200)
+        return reject(`[RadarJS] - ${req.status}: ${req.statusText}`);
+      var data = await req.json();
+      return resolve(data);
+    });
   }
 
-  /** 
+  /**
    * Fetches the Unix Epoch Timestamp of the last time the specified user voted for your bot.
    * @param {String} userID
    */
   lastVoted(userID) {
-    if(!userID) throw new Error("The user ID is required!")
+    if (!userID) throw new Error("The user ID is required!");
     return new Promise(async (resolve, reject) => {
-      var req = await this.fetch.default(`https://radarbotdirectory.xyz/lastvoted/${userID}/${this.client.user.id}`, {
-        method: "GET"
-      })
-      if(req.status != 200) return reject(`[RadarJS] - ${req.status}: ${req.statusText}`)
-      var data = await req.json()
-      return resolve(data)
-    })
+      var req = await this.fetch.default(
+        `https://radarbotdirectory.xyz/lastvoted/${userID}/${this.client.user.id}`,
+        {
+          method: "GET",
+        }
+      );
+      if (req.status != 200)
+        return reject(`[RadarJS] - ${req.status}: ${req.statusText}`);
+      var data = await req.json();
+      return resolve(data);
+    });
   }
-}
+};
